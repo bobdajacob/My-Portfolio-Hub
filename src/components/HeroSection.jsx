ab709817ece69ac99a3cbfdd1820984ae59a1af5
Bob <bobjacob23@yahoo.com>
2024-10-14 23:51:09 -0700

    added component files

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HeroSection() {
+return (
+  <div>
+    <h1>Welcome to my Portfolio Hub!</h1>
+    <p>This is my starting code to showcase my work!</p>
+  </div>
+  );
+}
+
+export default HeroSection;
